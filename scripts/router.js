a6c9752aa57090489b780a2bca60d0878138f3c8
Rick Tesmond <tesmonrd@gmail.com>
2015-12-22 12:08:07 -0800

    commented out pages that aren't used yet

--- scripts/router.js
+++ scripts/router.js
@@ -1,5 +1,9 @@
 page("/", trailsController.index );
-page("/mix", mixController.index);
-page("/trails", trailsController.index);
-page("/about", aboutController.index);
+
+// page("/mix", mixController.index);
+//
+// page("/trails", trailsController.index);
+//
+// page("/about", aboutController.index);
+
 page.start();
