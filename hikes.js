df439f9a693c2edd6515f3bf6bf6eda00432da50
Ardian Ajvazi <ardian_46@hotmail.com>
2015-12-24 16:58:21 -0800

    trails scroll. pop out modal for filtered trails renders all hikes

--- hikes.js
+++ hikes.js
@@ -45,16 +45,52 @@ Trail.populateTable = function (callback) {
 
 $(document).ready(function(){
   webDB.init();
+  var location ='';
+  var distance ='';
+  var elevation ='';
+  var dbQuery = '';
+  var rowHTML = '';
+
   $('.form-control').change(function() {
-    var string = 'SELECT * FROM trails WHERE '
-    var location = ($('.location:selected').val());
-    string += 'location = ' + "'" + location + "'";
-    var distance = ($('.distance:selected').val());
-    string += ' AND distance ' + distance;
-    var elevation =($('.elevation:selected').val());
-    string += ' AND elevation ' + elevation;
-    webDB.execute(string, function(tx) {
+
+    dbQuery = 'SELECT * FROM trails WHERE';
+    rowHTML = '';
+    location = ($('.location:selected').val());
+    if(location != ''){ dbQuery += ' location='+ '"' +location + '"'; }
+
+    distance = ($('.distance:selected').val());
+    if(distance != '' && location != ''){ dbQuery += ' AND distance ' + distance; }
+    else if(distance != ''){ dbQuery += ' distance ' + distance; }
+
+    elevation =($('.elevation:selected').val());
+    if(elevation != '' && distance != '' && location != ''){ dbQuery += ' AND elevation ' + elevation; }
+    else if(elevation != ''){ dbQuery += ' elevation ' + elevation; }
+
+  });// form-control
+
+  $('#findhike').on('click', function(){
+    if(location == '' && distance == '' && elevation == ''){ dbQuery = 'SELECT * FROM trails';}
+
+    webDB.execute(dbQuery, function(tx) {
+      //this is where we can add our random
+
+      if(tx.length == 0)
+      {
+        rowHTML += 'No records were found, please try another filter';
+      }
+      else {
+        for(var i=0; i<tx.length;i++){
+          if(i==0){ rowHTML += '<div class="row highlight">'; }
+          else {rowHTML += '<div class="row">';}
+          rowHTML += '<div class="col-md-12">';
+          rowHTML += '<h2><a target="_blank" href="'+ tx[i].url +'">'+ tx[i].name +'</a></h2>';
+          rowHTML += '<p>' + tx[i].description + '</p>';
+          rowHTML += '</div></div>';
+        }
+      }
+      $('.modal-body').html(rowHTML);
       console.log(tx);
     });
   });
+
 });
